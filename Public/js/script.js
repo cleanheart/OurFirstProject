374cd7bdc985a09158d1a588ef3d9dda1e0db546
cleanheart <heartclean737@gmail.com>
2023-09-15 19:03:44 +0600

    header sticky update

--- Public/js/script.js
+++ Public/js/script.js
@@ -9,4 +9,13 @@ $(function(){
       $(".header-top-wrapper").removeClass("sticky");
     }
   });
+
+  //search bar
+  const clearInput = () => {
+    const input = document.getElementsByTagName("input")[0];
+    input.value = "";
+  }
+  
+  const clearBtn = document.getElementById("clear-btn");
+  clearBtn.addEventListener("click", clearInput);
 });
